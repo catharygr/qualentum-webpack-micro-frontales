8b48bee538801f9d9373755b6b858fc6b4b7bf44
catharygr <103951447+catharygr@users.noreply.github.com>
2024-01-12 11:37:40 +0000

    Añadiendo una web component sustituyendo el titulo

--- src/web-component.js
+++ src/web-component.js
@@ -6,6 +6,8 @@ export class WebComponentTitle extends LitElement {
       font-size: 3.5rem;
       font-weight: 900;
       color: var(--primary-color);
+      padding: 1.5rem 0;
+      margin: 0;
     }
   `;
   constructor() {
